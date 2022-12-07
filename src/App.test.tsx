d200d98f5ca42d4fdf2867ed03a6694db179705f
BauTancredi <bautistatancredi@hotmail.com>
2022-12-08 00:18:23 +1100

    fiexed lint issues

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,17 @@
-import App from "./App";
+import { render } from '@testing-library/react';
 
-import { render } from "@testing-library/react";
+import App from './App';
 
 // test that the App component renders
-describe("App", () => {
-  test("renders App component", () => {
+describe('App', () => {
+  test('renders App component', () => {
     render(<App />);
   });
 
   // test that the App component renders the text "Hello World"
-  test("renders Hello World text", () => {
+  test('renders Hello World text', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText("Hello World")).toBeInTheDocument();
+    expect(getByText('Hello World')).toBeInTheDocument();
   });
 });

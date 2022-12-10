edc61802b9707abd1a1298b85dd0c36cf084b0ba
mateokeller <mateokellergms@gmail.com>
2022-12-10 15:18:29 -0300

    changed percentage to rem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header>
+    <header className="lg:hidden ">
       <div className=" w-full h-16 bg-gray-700 flex justify-between items-center">
-        <div className=" w-1/5 h-full bg-indigo-600 flex justify-center items-center rounded-r-2xl">
+        <div className=" w-20 h-full bg-indigo-600 flex justify-center items-center rounded-r-2xl">
           <object data="/src/assets/logo.svg"> </object>
         </div>
 
-        <div className="w-1/3 flex justify-between items-center ">
+        <div className="w-32 flex justify-between items-center ">
           <object data="/src/assets/icon-moon.svg"> </object>
           <hr className="h-16  border-0 bg-gray-600 w-px" />
           <img

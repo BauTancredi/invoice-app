1d3d303d5e75ad6abe553a0fc3ad70e21067dcbd
mateokeller <mateokellergms@gmail.com>
2023-01-18 19:52:26 -0300

    add user validation

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useCookies } from "react-cookie";
+import jwtDecode from "jwt-decode";
 
 const Drawer = () => {
+  const [cookie, setCookie] = useCookies(["user"]);
+  const { user } = jwtDecode(cookie.user);
+
+  const checkUser = () => {
+    console.log(user);
+
+    if (user) {
+      return true;
+    } else {
+      console.log("user not found");
+    }
+  };
+
   return (
     <div className=" max-w-3xl top-0 left-0 p-24 h-screen fixed w-full overflow-y-auto">
-      <div> Profile Drawer </div>
+      <div> {checkUser() ? <p>{user.name}</p> : <p>Please Log In</p>} </div>
     </div>
   );
 };

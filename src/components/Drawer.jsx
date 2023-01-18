67b602a9f80ac7109e83b86dfca49d36f3c036e9
mateokeller <mateokellergms@gmail.com>
2023-01-18 19:40:25 -0300

    add Drawer Component

---
+++ src/components/Drawer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Drawer = () => {
+  return (
+    <div className=" max-w-3xl top-0 left-0 p-24 h-screen fixed w-full overflow-y-auto">
+      <div> Profile Drawer </div>
+    </div>
+  );
+};
+
+export default Drawer;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import Drawer from "../Drawer.jsx";
 import Header from "../Header.jsx";
 
 export default function Layout() {
   return (
     <div className=" bg-light-100">
       <Header />
+      <Drawer />
       <div className="h-screen w-full flex items-center justify-center">
         <Outlet />
       </div>

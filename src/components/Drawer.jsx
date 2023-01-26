5f791ae228d3ba312e0a6aa6f40cf2a26f171c1d
mateokeller <mateokellergms@gmail.com>
2023-01-26 18:58:28 -0300

    add Drawer Animation

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 import jwtDecode from "jwt-decode";
 
-const Drawer = () => {
-  const [cookie, setCookie] = useCookies(["user"]);
-  const { user } = jwtDecode(cookie.user);
+const Drawer = (props) => {
+  // const [cookie, setCookie] = useCookies(["user"]);
+  // const { user } = jwtDecode(cookie.user);
 
-  const checkUser = () => {
-    if (user) {
-      return true;
-    } else {
-      console.log("user not found");
-    }
-  };
+  const user = { name: "Ney" };
+
+  const logOut = () => {
+    // onClick
+  }
 
   return (
-    <div className="drawer-animation max-w-3xl top-0 left-0 p-24 h-screen fixed w-full overflow-y-auto border-8 bg-slate-400 z-10">
-      <div> {checkUser() ? <p>{user.name}</p> : <p>Please Log In</p>} </div>
+    <div className={props.styles}>
+      {/* profile details  or invoice details component */}
+      <div alt="profile-component" className=" pt-10 px-12 font-bold text-3xl h-screen">
+        <div>{user ? <p>{user.name}</p> : <p>Please Log In</p>}</div>
+        <button
+          className="w-50 mt-12 my-4 bg-primary-400 p-2 text-gray-100 font-bold rounded hover:bg-primary-500 cursor-pointer text-sm"
+          onClick={logOut}
+        >
+          {user ? "Log Out" : "Log In"}
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,7 +5,7 @@ import Drawer from "../Drawer.jsx";
 import Header from "../Header.jsx";
 
 export default function Layout() {
-  const [drawer, setOpenDrawer] = useState(false);
+  const [drawer, setOpenDrawer] = useState(true);
 
   const toggleDrawer = () => {
     if (!drawer) {
@@ -17,9 +17,23 @@ export default function Layout() {
   };
 
   return (
-    <div className=" bg-light-100">
+    <div>
       <Header handleClick={toggleDrawer} />
-      {!drawer ? <Drawer /> : null}
+      <div className={!drawer ? "opacity w-full" : ""}>
+        {!drawer ? (
+          <Drawer
+            styles={
+              "drawer-animation-toggle max-w-3xl top-0 left-0 h-screen fixed w-full overflow-y-auto rounded-r-3xl bg-white z-10 pl-20 shadow-sm"
+            }
+          />
+        ) : (
+          <Drawer
+            styles={
+              "drawer-animation max-w-3xl top-0 left-0 h-screen fixed w-full overflow-y-auto rounded-r-3xl bg-white z-10 pl-20 shadow-sm"
+            }
+          />
+        )}
+      </div>
       <div className="h-screen w-full flex items-center justify-center">
         <Outlet />
       </div>
